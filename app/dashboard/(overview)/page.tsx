9835df0a78d78226c671ce911247e85a04a5082b
tomas.unzain <tomiunzv@gmail.com>
2024-05-08 16:46:30 -0400

    ultima parte del curso

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,12 @@ import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+  };
  
 export default async function Page() {
     const {
